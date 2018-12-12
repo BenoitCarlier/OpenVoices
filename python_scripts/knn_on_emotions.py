34c43983aa46fcc2bfec6b8093d6bf3c9b257d2c
Pixel-Jack <pontclement@yahoo.com>
2018-12-12 13:26:04 +0100

    Refonte pyAudio and begin of visualisation tools

--- python_scripts/knn_on_emotions.py
+++ python_scripts/knn_on_emotions.py
@@ -1,26 +1,25 @@
 from pyAudioAnalysis import audioTrainTest as aT
 import os
 
-emotion_list = []
-emotion_list.append('angry')
-emotion_list.append('calm')
-emotion_list.append('neutral')
-emotion_list.append('happy')
-emotion_list.append('disgust')
-emotion_list.append('fearful')
-emotion_list.append('surprised')
+from python_scripts import tools
+
+emotion_list = tools.MAP_EMOTION.values()
+# emotion_list = []
+# emotion_list.append('angry')
+# emotion_list.append('calm')
+# emotion_list.append('neutral')
+# emotion_list.append('happy')
+# emotion_list.append('disgust')
+# emotion_list.append('fearful')
+# emotion_list.append('surprised')
 
 input_dir = 'output_by_emotion'
 
+output_dir = 'Models'
+output_model_name = 'knnEmotion7'
+
 path_base = os.path.join(os.path.dirname(os.getcwd()), input_dir)
+dir_list = [os.path.join(path_base, emo) for emo in emotion_list]
 
-dir_list = []
-
-for emo in emotion_list:
-    dir_list.append(os.path.join(path_base,emo))
-
-for dir in dir_list:
-    files = os.listdir(dir)
-    print(files)
-
-aT.featureAndTrain(dir_list, 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "knn", "knnEmotion7")
+aT.featureAndTrain(dir_list, 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "knn",
+                   os.path.join(output_dir, output_model_name))
